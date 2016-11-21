7c059b120f5c7dbbc4b00a93e36698ad45154dde
Sinan Şahin <bote.sinansahin@gmail.com>
2016-11-22 02:44:56 +0300

    ticket status modeli oluşturuldu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ticket_statuses
   resources :ticket_categories
   resources :ticket_types
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121232942) do
+ActiveRecord::Schema.define(version: 20161121233121) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161121232942) do
   end
 
   create_table "ticket_categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "ticket_statuses", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
