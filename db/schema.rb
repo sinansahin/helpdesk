51438bde1594ac7970ab12af671a37103d2bc213
Sinan Şahin <bote.sinansahin@gmail.com>
2016-11-22 02:29:13 +0300

    ticket type modeli oluşturuldu.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121231742) do
+ActiveRecord::Schema.define(version: 20161121232750) do
 
   create_table "profiles", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "ticket_types", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
