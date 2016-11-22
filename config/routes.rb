bb8c1c3b3c08a2e0ee4cb74bd823a45dee0ba5a6
Sinan Şahin <bote.sinansahin@gmail.com>
2016-11-22 03:00:36 +0300

    organization modeli oluşturuldu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :organizations
   resources :ticket_task_assignments
   resources :ticket_tasks
   resources :ticket_messages
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121235350) do
+ActiveRecord::Schema.define(version: 20161121235905) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "parent_id",  limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name",       limit: 255
